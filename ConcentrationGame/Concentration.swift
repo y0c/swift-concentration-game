b2d376e9e2bfd8b5524d878a0573a859a83304b4
LimHoSung <wonder789@naver.com>
2018-02-25 00:48:43 +0900

    refactor indexOfAndOnlyFaceUpCard

--- ConcentrationGame/Concentration.swift
+++ ConcentrationGame/Concentration.swift
@@ -12,27 +12,38 @@ class Concentration {
     
     var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            //뒤집힌 카드가 하나일경우 해당 index 있을경우 nil
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = ( index == newValue )
+            }
+        }
+    }
     
     func chooseCard(at index: Int) {
         
         if !cards[index].isMatched {
-            
-            
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
                 if cards[matchIndex].identifier == cards[index].identifier {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
-                
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
             
